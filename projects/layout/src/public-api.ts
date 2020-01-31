3e6aa83b7d746a1f37f484fda5f961e442f76b94
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-01-31 15:37:49 -0300

    Arquivos fontes da Biblioteca Layout

---
+++ projects/layout/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of layout
+ */
+
+export * from './lib/layout.service';
+export * from './lib/layout.component';
+export * from './lib/header/header.component';
+export * from './lib/footer/footer.component';
+export * from './lib/menu/menu.component';
+export * from './lib/layout.module';
