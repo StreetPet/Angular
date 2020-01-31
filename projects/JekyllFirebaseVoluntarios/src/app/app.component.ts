fcbbc04ed8e6b855a4d5a0fd93cf1a05184f5c65
Carlos Delfino - Sony 1 <consultoria@carlosdelfino.eti.br>
2020-01-31 04:28:25 -0300

    Criado Repositório

---
+++ projects/JekyllFirebaseVoluntarios/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit {
+  title = 'Voluntarios';
+
+  ngOnInit() { }
+}
